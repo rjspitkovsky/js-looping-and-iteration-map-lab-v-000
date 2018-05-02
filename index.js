778061fb3e8c34ad9809e0c3079bdb6968d92441
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-02 21:50:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function lowerCaseDrivers(drivers) {
 }
 
 function nameToAttributes(drivers) {
-  return drivers.map(function (name) {return Object.assign({}, name, {firstName: name.split(" ")[0], lastName: name.split(" ")[1]})})
+  return drivers.map(function (name) {return Object.assign({}, name, {firstName: name.toLowerCase().split(" ")[0], lastName: name.toLowerCase.split(" ")[1]})})
 }
 
 function attributesToPhrase(drivers) {
